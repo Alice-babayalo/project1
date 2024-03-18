0a6953a1d6569b4aa878e8345aac7b2d98279cd0
Alice-babayalo <umuhozawasealice@gmail.com>
2024-03-18 16:03:39 +0200

    coding is fun!

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ console.log(__dirname);
 const addition=(a,b)=>{
     return a+b;
 }
-console.log(addition(5,95));
-console.log(" This is my first time pushing on my github account \n So I am so happy and I am going to do all\n these projects as a form of fun thanks to github!")
+console.log(" "+addition(5,95)+"%");
+console.log(" This is my first time pushing on my github account \n So I am so happy and I am going to do all\n these projects as a form of fun thanks to github!");
+console.log(" I can't imagine that I am just coding for fun with my typing skills! \n CODDING IS FUN")
