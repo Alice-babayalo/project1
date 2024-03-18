e1759e34e1adc659e37eb99d71a77ba709df7d32
Alice-babayalo <umuhozawasealice@gmail.com>
2024-03-18 16:00:02 +0200

    a testimony of happiness

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ const addition=(a,b)=>{
     return a+b;
 }
 console.log(addition(5,95));
+console.log(" This is my first time pushing on my github account \n So I am so happy and I am going to do all\n these projects as a form of fun thanks to github!")
