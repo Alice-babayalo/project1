0e5567c2aaa2ab954e789d404f6f309a22903402
Alice-babayalo <umuhozawasealice@gmail.com>
2024-03-18 15:56:16 +0200

    Adding an additional funtionality

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const name="Alice Babayalo";
 console.log("Hello, "+name+"!");
-console.log(__dirname)
+console.log(__dirname);
+const addition=(a,b)=>{
+    return a+b;
+}
+console.log(addition(5,95));
